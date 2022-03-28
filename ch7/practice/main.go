be7090e0f4e2d7ea789062bee2c502d1ecb7235a
tribhuvan <tribhuvanreddyramidi@gmail.com>
2022-03-28 12:20:57 +0530

    Ex: 7.11

--- ch7/practice/main.go
+++ ch7/practice/main.go
@@ -4,6 +4,10 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"os"
+	"sort"
+	"text/tabwriter"
+	"time"
 
 	"golang.org/x/net/html"
 )
@@ -12,6 +16,18 @@ import (
 type StringReader struct {
     s string
     i int64
+}
+
+type Profile struct {
+	Name string
+	Age int
+	Mobile string
+}
+
+var pro = []*Profile{
+	{"Tribhvan",31, "7760808111",},
+	{"Tribhvan1",33, "7760808112",},
+	{"Tribhvan2",34, "7760808113",},
 }
 
 func (r *StringReader) Read(p []byte) (n int, err error) {
@@ -35,13 +51,6 @@ func NewReader(s string) *StringReader {
 
 
 func main()  {
-	// NewReader("Hi")
-
-	//var sr StringReader
-	//x:= NewReader("<p>Links:</p><div>Hi</div>")
-	// fmt.Println("x ",x.s)
-	// sr.Read([]byte(x.s))
-	// x:=NewReader("")
 	doc, err := html.Parse(NewReader("<p>Links:</p><div><a href='xxx'>Hi</a></div>"))
 	if err != nil {
 		panic(err)
@@ -57,8 +66,12 @@ func main()  {
 	//fmt.Println("r.r: ", r.r)
 		
 	}
+	fmt.Println("By Title, Artist")
+	printTracks(useSortByColumns())
 
-	
+	fmt.Println("\nUse sort.Stable. By Title, Artist")
+	printTracks(useSortStable())
+
 }
 
 func visit(links []string, n *html.Node) []string {
@@ -108,3 +121,134 @@ func LimitReader(r io.Reader, n int64) io.Reader {
 // func (l *LimitReaderType)Close()(err error)  {
 // 	return l.cl.Close()
 // }
+
+
+
+// Sorting sorts a music playlist into a variety of orders.
+var stdout io.Writer = os.Stdout
+
+//!+main
+type Track struct {
+	Title  string
+	Artist string
+	Album  string
+	Year   int
+	Length time.Duration
+}
+
+func tracks() []*Track {
+	return []*Track{
+		{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+		{"Go", "Moby", "Moby", 1992, length("3m37s")},
+		{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+		{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+	}
+}
+
+func length(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(s)
+	}
+	return d
+}
+
+//!-main
+
+//!+printTracks
+func printTracks(tracks []*Track) {
+	const format = "%v\t%v\t%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+	fmt.Fprintf(tw, format, "-----", "------", "-----", "----", "------")
+	for _, t := range tracks {
+		fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
+	}
+	tw.Flush() // calculate column widths and print table
+}
+
+//!-printTracks
+
+//!+titlecode
+type byTitle []*Track
+
+func (x byTitle) Len() int           { return len(x) }
+func (x byTitle) Less(i, j int) bool { return x[i].Title < x[j].Title }
+func (x byTitle) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+//!-titlecode
+
+//!+artistcode
+type byArtist []*Track
+
+func (x byArtist) Len() int           { return len(x) }
+func (x byArtist) Less(i, j int) bool { return x[i].Artist < x[j].Artist }
+func (x byArtist) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+//!-artistcode
+
+//!+yearcode
+type byYear []*Track
+
+func (x byYear) Len() int           { return len(x) }
+func (x byYear) Less(i, j int) bool { return x[i].Year < x[j].Year }
+func (x byYear) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+//!-yearcode
+
+//!+multicolumns
+
+type less func(x, y *Track) bool
+
+func colTitle(x, y *Track) bool  { return x.Title < y.Title }
+func colArtist(x, y *Track) bool { return x.Artist < y.Artist }
+func colAlbum(x, y *Track) bool  { return x.Album < y.Album }
+func colYear(x, y *Track) bool   { return x.Year < y.Year }
+func colLength(x, y *Track) bool { return x.Length < y.Length }
+
+type byColumns struct {
+	tracks  []*Track
+	columns []less
+}
+
+func sortByColumns(t []*Track, f ...less) *byColumns {
+	return &byColumns{
+		tracks:  t,
+		columns: f,
+	}
+}
+
+func (x byColumns) Len() int      { return len(x.tracks) }
+func (x byColumns) Swap(i, j int) { x.tracks[i], x.tracks[j] = x.tracks[j], x.tracks[i] }
+func (x byColumns) Less(i, j int) bool {
+	a, b := x.tracks[i], x.tracks[j]
+	var k int
+	// compare columns one by one except the last
+	for k = 0; k < len(x.columns)-1; k++ {
+		f := x.columns[k]
+		switch {
+		case f(a, b):
+			return true
+		case f(b, a):
+			return false
+		}
+	}
+	// all equal, use last column as final judgement
+	return x.columns[k](a, b)
+}
+
+//!-multicolumns
+
+func useSortByColumns() []*Track {
+	t := tracks()
+	sort.Sort(sortByColumns(t, colTitle, colArtist))
+	return t
+}
+
+func useSortStable() []*Track {
+	t := tracks()
+	sort.Stable(byArtist(t))
+	sort.Stable(byTitle(t))
+	return t
+}
+
